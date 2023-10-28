83b938aff52290f2ecc5b92353364c59f6260fcf
Raisa Azad <raisaazad55@gmail.com>
2023-10-28 23:57:04 +0600

    Strings

---
+++ concatenating_name.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 28 23:43:28 2023
+
+@author: User
+"""
+
+first_name = "raisa"
+last_name = "azad"
+full_name = first_name+ ' '+last_name
+print(full_name)
+print("Hello"+ " "+full_name.title()+"!")
---
+++ name.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 28 23:24:45 2023
+
+@author: User
+"""
+
+name = "raisa azad"
+print(name.title())
+
+print(name.upper())
+print(name.lower())
