65cf91459d870cf68c4b8ea8f95e5d7ca8737387
Raisa Azad <raisaazad55@gmail.com>
2023-10-29 00:25:55 +0600

    Numbers

---
+++ birthday.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 29 00:19:57 2023
+
+@author: User
+"""
+
+age = 26
+print("Happy"+" "+str(age)+"th Birthday!")
+
+print(5+3)
+print(8-4)
+print(4*2)
+print(32/4)
