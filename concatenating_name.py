41bf5e88e5d2c1bee93107415ec25717dd8149b1
Raisa Azad <raisaazad55@gmail.com>
2023-10-29 00:19:20 +0600

    Strings

--- concatenating_name.py
+++ concatenating_name.py
@@ -7,6 +7,6 @@ Created on Sat Oct 28 23:43:28 2023
 
 first_name = "raisa"
 last_name = "azad"
-full_name = first_name+ ' '+last_name
+full_name = first_name+' '+last_name
 print(full_name)
 print("Hello"+ " "+full_name.title()+"!")
---
+++ tabs and new lines.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 29 00:07:27 2023
+
+@author: User
+"""
+
+famous_person = "\talbert einstein"
+print(famous_person.title()+" "+ "once said,"+"\n'A person never made a mistake never tried anything new'")
