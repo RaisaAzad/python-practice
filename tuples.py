21bf4b9818b7620a7613f6f4f0bcefca17d62f3e
Raisa Azad <raisaazad55@gmail.com>
2023-10-29 16:44:46 +0600

    Tuples

---
+++ tuples.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 29 16:41:43 2023
+
+@author: User
+"""
+
+dimensions = (200, 50)
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+    
+dimensions = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
