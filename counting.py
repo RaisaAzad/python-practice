cb71072dc3aff1825b3ce74049db19d47c66011c
Raisa Azad <raisaazad55@gmail.com>
2023-10-30 21:47:58 +0600

    While loop

---
+++ counting.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 21:45:02 2023
+
+@author: User
+"""
+
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+current_number += 2
