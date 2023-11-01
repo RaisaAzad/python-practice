5801e49d460af222c7bcd6c2a061a0f424c53285
Raisa Azad <raisaazad55@gmail.com>
2023-11-01 23:24:44 +0600

    Functions

---
+++ display_message_function.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  1 22:46:24 2023
+
+@author: User
+"""
+
+def display_message():
+    print("I am lerning about functions")
+    
+display_message()
---
+++ favorite_book_function.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  1 23:22:08 2023
+
+@author: User
+"""
+
+def favorite_book(title):
+    print("One of my favorite books is "+ title)
+    
+favorite_book("Kafka on the Shore")
